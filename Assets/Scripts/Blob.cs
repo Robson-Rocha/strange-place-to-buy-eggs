44e0cddff9939420c89c341f0ae13e12890276c5
Robson Rocha de Araújo <rrocha.araujo@gmail.com>
2026-03-07 22:18:09 +0100

    Add Blob NPC assets, animations and script
    Add a new Blob NPC including Aseprite source files and PNGs, sprite imports, animation clips (Idle, Walk Left/Right, Hit Left/Right), animator controller, and the Blob.cs script. Assets are organized under Assets/Sprites/NPCs/Blob and Assets/Animations/Blob with corresponding .meta files to register them in Unity. This brings the visual and animation setup needed for the Blob character and includes the runtime script to drive its behavior.

---
+++ Assets/Scripts/Blob.cs
@@ -0,0 +1,27 @@
+﻿using RobsonRocha.UnityCommon;
+using UnityEngine;
+
+[RequireComponent(typeof(Moveable))]
+[RequireComponent(typeof(AnimatorParameterBinder))]
+[RequireComponent(typeof(Knockbackable))]
+[RequireComponent(typeof(AiBehaviourController))]
+public class Blob : MonoBehaviour
+{
+    private AnimatorParameterBinder _animatorParameterBinder;
+    private Moveable _moveable;
+    private Knockbackable _knockbackable;
+
+    void Awake()
+    {
+        if (this.TryInitComponent(ref _moveable) &&
+            this.TryInitComponent(ref _knockbackable) &&
+            this.TryInitComponent(ref _animatorParameterBinder))
+        {
+            _animatorParameterBinder.Bind(Consts.ANIM_PARAM_IS_MOVING, () => !_knockbackable.IsKnockingBack && _moveable.IsMoving);
+            _animatorParameterBinder.Bind(Consts.ANIM_PARAM_IS_IDLING, () => !_knockbackable.IsKnockingBack && _moveable.IsIdling);
+            _animatorParameterBinder.Bind(Consts.ANIM_PARAM_IS_FACING_LEFT, () => _moveable.IsFacingLeft);
+            _animatorParameterBinder.Bind(Consts.ANIM_PARAM_IS_FACING_RIGHT, () => _moveable.IsFacingRight);
+            _animatorParameterBinder.Bind(Consts.ANIM_PARAM_IS_KNOCKING_BACK, () => _knockbackable.IsKnockingBack);
+        }
+    }
+}
